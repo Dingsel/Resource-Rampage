c2e6e0cda15774f1f69f3b5fce0875e04ce1b93c
Dingsel <95553908+Dingsel@users.noreply.github.com>
2023-03-22 22:19:31 +0100

    waves Co-authored-by: iBlqzed <iBlqzed@users.noreply.github.com>

--- packs/BP/scripts/gameplay/runtime/entities/death.js
+++ packs/BP/scripts/gameplay/runtime/entities/death.js
@@ -2,10 +2,10 @@ import { MolangVariableMap, Player, world } from "@minecraft/server";
 
 const map = new MolangVariableMap()
 
-events.entityDie.subscribe(({ deadEntity }) => {
+events.entityDie.subscribe(({ deadEntity, damageSource }) => {
     if (deadEntity instanceof Player) return
     deadEntity.dimension.spawnParticle("dest:coin", deadEntity.location, map)
-    coins++
+    console.warn(damageSource.cause)
 }, {
     entityTypes: ((types = []) => {
         //for non dest entities??
--- packs/BP/scripts/gameplay/runtime/entities/waveSystem.js
+++ packs/BP/scripts/gameplay/runtime/entities/waveSystem.js
@@ -1,104 +1,84 @@
 import { world } from "@minecraft/server"
 
-export class EnemySpawner {
-	/**
-	 * @param {Wave[]} waves
-	 */
-	constructor(waves = []) {
-		this.waves = waves
+const spawnPoints = Object.entries({
+	"x+": { x: 260, y: 75, z: 95 },
+	"x-": { x: -120, y: 75, z: 28 },
+	"z+": { x: 120, y: 75, z: 275 },
+	"z-": { x: 70, y: 74, z: -100 }
+})
+
+
+const randNum = (min, max) => Math.floor(Math.random() * (max - min)) + min
+
+class Mob {
+	constructor(id, weight, minWave) {
+		this.id = id
+		this.weight = weight
+		this.minWave = minWave
 	}
-	/**
-	 * @param {Wave} wave
-	 * @returns {EnemySpawner}
-	 */
-	addWave(wave) {
-		this.waves.push(wave)
-		return this
+}
+
+export class EnemySpawner {
+	constructor() {
+		this.waveNumber = 1
 	}
 	/**
 	 * @returns {Wave}
 	 */
 	nextWave() {
-		return this.waves.shift()
+		return new Wave(this.waveNumber++)
 	}
 }
 
-export class Wave {
-	/**
-	 * @param {WaveOptions} options
-	 */
-	constructor(options) {
-		this.options = options
+class Wave {
+	/**@type {Mob[]} */
+	static enemies = [
+		new Mob("minecraft:zombie", 1, 0),
+		new Mob("minecraft:skeleton", 1, 2)
+	]
+	/** @param {number} waveNumber */
+	constructor(waveNumber) {
+		this.waveNumber = waveNumber
+		this.enemyDirections = this.directions()
 	}
+	/**@returns {[Mob, import("@minecraft/server").Vector3]} */
 	*generateEnemies() {
-		for (const option of Object.entries(this.options.enemies)) {
-			for (let i = 0; i < option.amount; i++) {
-				yield Array.from({ length: option.enemyAmount }).map(() => world.overworld.spawnEntity(option.id, this.options.location))
+		const availableMobs = Wave.enemies.filter((mob) => mob.minWave <= this.waveNumber)
+		const totalWeight = availableMobs.reduce((total, mob) => total + mob.weight, 0);
+		const mobCount = this.waveNumber * 5
+		console.warn(mobCount)
+		for (let i = 0; i < mobCount; i++) {
+			let randomWeight = Math.floor(Math.random() * totalWeight);
+			for (let j = 0; j < availableMobs.length; j++) {
+				randomWeight -= availableMobs[j].weight;
+				if (randomWeight < 0) {
+
+					yield [availableMobs[j].id, this.enemyDirections[randNum(0, this.enemyDirections.length - 1)][1]];
+					break;
+				}
 			}
 		}
 	}
-}
-
-/**
- * @typedef {{ location: Vector3, enemies: [{ id: string, amount: number, enemyAmount: number }] }} WaveOptions
- */
-
-
-class Mob {
-	constructor(id, minWave, weight) {
-		this.id = id
-		this.minWave = minWave
-		this.weight = weight
-	}
-}
-
-
-const mobTypes = [
-	new Mob("minecraft:zombie", 0, 100),
-	new Mob("minecraft:skeleton", 2, 1)
-]
-
-
-
-function generateWave(roundNumber) {
-	const waveOptions = { location: { x: 37, y: 130, z: 68 }, enemies: [] };
-
-	// Determine which types of mobs can spawn in this wave
-	const availableMobs = mobTypes.filter((mob) => mob.minWave <= roundNumber);
-
-	// Calculate the total weight of available mobs
-	const totalWeight = availableMobs.reduce((total, mob) => total + mob.weight, 0);
-
-	// Choose a random subset of mobs to spawn in this wave based on their weights
-	const mobCount = roundNumber * 5
-	for (let i = 0; i < mobCount; i++) {
-		let randomWeight = Math.floor(Math.random() * totalWeight);
-		let chosenMob;
-		for (let j = 0; j < availableMobs.length; j++) {
-			randomWeight -= availableMobs[j].weight;
-			if (randomWeight < 0) {
-				chosenMob = availableMobs[j];
-				chosenMob.amount = 1
-				chosenMob.enemyAmount = 1
-				break;
-			}
+	/**@private */
+	directions() {
+		let from = []
+		const directions = Math.floor((this.waveNumber + 4) / 5)
+		for (let i = 0; i < directions; i++) {
+			const randInt = Math.floor(Math.random() * 4)
+			const v = spawnPoints[randInt]
+			console.warn("Coming from " + v[0])
+			from.push(v)
 		}
-		waveOptions.enemies.push(chosenMob);
+		return from
 	}
-
-	return waveOptions;
 }
 
-function asyncTickTimeout(delay) {
-	return new Promise((resolve) => { system.runTimeout(resolve, delay) })
+const spawner = new EnemySpawner()
+spawner.waveNumber = 6
+const wave = spawner.nextWave()
+
+for (const [enemy, location] of wave.generateEnemies()) {
+	//console.warn(enemy, location.x)
+	runCommand(`summon ${enemy} ${location.x} ${location.y} ${location.z}`)
+	runCommand(`execute as @e[type=${enemy},x=${location.x},y=${location.y},z=${location.z},c=1] at @s run spreadplayers ~ ~ 10 11 @s`)
 }
-
-let options = generateWave(3)
-
-
-;(async function spawn(){
-	for (const e of options.enemies) {
-		world.overworld.spawnEntity(e.id, options.location)
-		await asyncTickTimeout(100)
-	}
-})
