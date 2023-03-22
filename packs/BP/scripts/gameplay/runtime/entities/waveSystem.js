eee2991314d694af2334ad75ba5f2bdd0012f5bb
Dingsel <95553908+Dingsel@users.noreply.github.com>
2023-03-22 20:10:42 +0100

    Wave

--- packs/BP/scripts/gameplay/runtime/entities/waveSystem.js
+++ packs/BP/scripts/gameplay/runtime/entities/waveSystem.js
@@ -42,3 +42,63 @@ export class Wave {
 /**
  * @typedef {{ location: Vector3, enemies: [{ id: string, amount: number, enemyAmount: number }] }} WaveOptions
  */
+
+
+class Mob {
+	constructor(id, minWave, weight) {
+		this.id = id
+		this.minWave = minWave
+		this.weight = weight
+	}
+}
+
+
+const mobTypes = [
+	new Mob("minecraft:zombie", 0, 100),
+	new Mob("minecraft:skeleton", 2, 1)
+]
+
+
+
+function generateWave(roundNumber) {
+	const waveOptions = { location: { x: 37, y: 130, z: 68 }, enemies: [] };
+
+	// Determine which types of mobs can spawn in this wave
+	const availableMobs = mobTypes.filter((mob) => mob.minWave <= roundNumber);
+
+	// Calculate the total weight of available mobs
+	const totalWeight = availableMobs.reduce((total, mob) => total + mob.weight, 0);
+
+	// Choose a random subset of mobs to spawn in this wave based on their weights
+	const mobCount = roundNumber * 5
+	for (let i = 0; i < mobCount; i++) {
+		let randomWeight = Math.floor(Math.random() * totalWeight);
+		let chosenMob;
+		for (let j = 0; j < availableMobs.length; j++) {
+			randomWeight -= availableMobs[j].weight;
+			if (randomWeight < 0) {
+				chosenMob = availableMobs[j];
+				chosenMob.amount = 1
+				chosenMob.enemyAmount = 1
+				break;
+			}
+		}
+		waveOptions.enemies.push(chosenMob);
+	}
+
+	return waveOptions;
+}
+
+function asyncTickTimeout(delay) {
+	return new Promise((resolve) => { system.runTimeout(resolve, delay) })
+}
+
+let options = generateWave(3)
+
+
+;(async function spawn(){
+	for (const e of options.enemies) {
+		world.overworld.spawnEntity(e.id, options.location)
+		await asyncTickTimeout(100)
+	}
+})()
