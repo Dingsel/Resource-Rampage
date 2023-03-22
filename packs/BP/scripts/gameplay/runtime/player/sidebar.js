e525b4c55a27b8e6e5784dd4f1f2d96059d804ce
Remember M9 <55829656+Myselfnt@users.noreply.github.com>
2023-03-22 14:03:10 -0700

    Update sidebar.js

--- packs/BP/scripts/gameplay/runtime/player/sidebar.js
+++ packs/BP/scripts/gameplay/runtime/player/sidebar.js
@@ -1,10 +1,11 @@
-import { InfoMapProperties } from "resources";
+import { ScreenDisplay } from "@minecraft/server";
+import { TowerDefenition } from "gameplay/building";
 import { GameDatabase } from "utils";
 
 const {
-    parse, stringify, Array: { from: map, isArray }
+    parse, stringify, Array: { from: map, isArray }, Object: { assign }
 } = {
-    parse: JSON.parse.bind(JSON), stringify: JSON.stringify.bind(JSON), Array
+    parse: JSON.parse.bind(JSON), stringify: JSON.stringify.bind(JSON), Array, Object
 };
 
 system.events.gameInitialize.subscribe(() => {
@@ -13,10 +14,45 @@ system.events.gameInitialize.subscribe(() => {
 
             player.onScreenDisplay.setTitle([
                 ``,
-                `Coins: ${global.infoMap.get(InfoMapProperties.coins)}`
+                `Coins: ${coins}`
             ].join('\n'))
 
+            const { onScreenDisplay, nameTag, scores, selectedTower } = player;
+            const playerInfo = { nameTag, scores, selectedTower }
+            setDisplay(onScreenDisplay,playerInfo)
         }
-    }, 15)
+    }, 10)
 });
+
+/**
+ * @param {ScreenDisplay} screen
+ * @param {playerInfo} playerInfo
+ * */
+async function setDisplay(screen, playerInfo) {
+    const {nameTag} = playerInfo
+    assign(this, {
+        setTitle:[
+            , ,
+            `§gCoin${coins > 1 ? 's' : ''}§8:§r ${coins.unitFormat(1, '§g')}`,
+            ``,
+        ],
+        setActionBar: [
+
+        ]
+    })
+    for (const k in this) screen[k](this[k].join('\n'))
+    return true;
+}
+const d = GameDatabase.getDatabase(objectives.gameplay).entries()
+// print(stringify(d))
+
 export { };
+
+
+
+/**
+ * @typedef {Object} playerInfo
+ * @property {TowerDefenition} selectedTower
+ * @property {string} nameTag
+ * @property {{[key:string]:number}} scores
+ */
