2ba562cfe537913b6273e78783efdb0766e158d6
ConMaster2112 <samdembiny@gmail.com>
2023-03-25 00:10:29 +0100

    death screen improved

--- packs/BP/scripts/gameplay/runtime/player/camera.js
+++ packs/BP/scripts/gameplay/runtime/player/camera.js
@@ -1,53 +1,35 @@
-import { GameMode, MinecraftEffectTypes, MinecraftEntityTypes, MolangVariableMap, Player, system, Vector, world, World } from "@minecraft/server";
-import { ArcherTowerLevelStructure } from "resources";
+import { Effect, GameMode, MinecraftEffectTypes, MinecraftEntityTypes, MolangVariableMap, Player, system, Vector, world, World } from "@minecraft/server";
+import { ArcherTowerLevelStructure, centerLocation } from "resources";
 import { ImpulseParticlePropertiesBuilder, SquareParticlePropertiesBuilder } from "utilities/MolangVariableMaps";
 
-
-events.playerSpawn.subscribe(async ({ player, initialSpawn }) => {
-    if (!initialSpawn) {
-        try {
-        } catch (error) {
-            errorHandle(error);
-        }
-    }
-});
+const center = centerLocation;
 events.entityDie.subscribe(async ({ deadEntity }) => {
-    const { location, dimension } = deadEntity;
-    deadEntity.setSpawn(location, dimension);
-    await deadEntity.setGameMode(GameMode.spectator);
+    deadEntity.runCommandAsync('fog @s push dest:death_fog fog');
+    deadEntity.setGameMode(GameMode.spectator);
     deadEntity.setMovementPermission(false);
     deadEntity.setCameraPermission(false);
-    await sleep(3);
-    const loc1 = {x:82,y:100,z:52};
-    let last = -1;
-    let pass = true;
     for (let loc of deathScreen())
     {
-        if(deadEntity.health > 0 && pass) {
-            last = 15;pass = false;
-        }
-        if (last == 5){
-            deadEntity.addEffect(MinecraftEffectTypes.blindness,20,5,false);
-            deadEntity.setGameMode(global.config.defualt_game_mode);
-        }
-        if (last-- == 0) break;
-        deadEntity.teleportFacing(loc,overworld,loc1);
         await nextTick;
+        if(!deadEntity.isOnline) return;
+        if(deadEntity.health > 0) break;
+        deadEntity.teleportFacing(loc,overworld,center);
     }
+    await deadEntity.setGameMode(global.config.defualt_game_mode);
     await deadEntity.setMovementPermission(true);
     await deadEntity.setCameraPermission(true);
-    deadEntity.teleportFacing(world.getDefaultSpawnPosition(),overworld,loc1);
+    await deadEntity.runCommandAsync('fog @s remove fog');
+    deadEntity.teleportFacing(world.getDefaultSpawnPosition(),overworld,center);
 }, { entityTypes: [MinecraftEntityTypes.player.id] })
 
-function* deathScreen(){
-    const loc2 = {x:82,y:105,z:52};
-    while(true) for (let loc of MovingPath(150,300,15,Math.random()*360,0,Math.random()>0.5?1:-1).offSet(loc2)) yield loc;
+function* deathScreen(r=175,angle = 15){
+    while(true) for (let loc of MovingPath(r * (Math.random()+0.5),angle * 15,angle,Math.random()*360,(Math.random()-0.5)/20,(Math.random()>=0.5)?1:-1).offSet(Vector.add(center,{x:0,y:Math.random()*20,z:0}))) yield loc;
 }
 
 function* MovingPath(radius = 30, steps = 1,angle = 360, offset=0, motion=0, direction = 1){
     const max = angle*Math.PI / 180;
     const ofset = offset*Math.PI/180;
-    for (let x = 0, y = 0, z = 0, i = 0, m = 1; i < max; i+= max/steps*m, x = Math.cos((i+ofset)*direction) * radius, z = Math.sin((i+ofset)*direction) * radius, y = 0, m+=motion) 
+    for (let x = radius, y = 0, z = 0, i = 0, m = 1; i < max; i+= max/steps*(Math.abs(m)), x = Math.cos((i+ofset)*direction) * radius, z = Math.sin((i+ofset)*direction) * radius, m+=motion) 
     { 
         yield {x,y,z};
     }
--- packs/BP/scripts/gameplay/runtime/player/onJoin.js
+++ packs/BP/scripts/gameplay/runtime/player/onJoin.js
@@ -1,12 +1,17 @@
 import { ItemLockMode, ItemStack } from "@minecraft/server";
-import { MenuItemStacks } from "resources";
+import { MenuItemStacks, centerLocation } from "resources";
 
 for (const p of world.getPlayers()) {
     p.container.setItem(8,MenuItemStacks.Menu);
 }
-events.playerSpawn.subscribe(({player,initialSpawn})=>{
-    if(!initialSpawn) return;
-    player.teleport(global.config.default_spawn_point,overworld,0,0);
+events.playerSpawn.subscribe(async ({player,initialSpawn})=>{
+    if(!initialSpawn) return; 
+    player.teleportFacing(global.config.default_spawn_point,overworld,centerLocation);
     player.container.setItem(8,MenuItemStacks.Menu);
-    player.setGameMode(global.config.defualt_game_mode);
+    await player.setGameMode(global.config.defualt_game_mode);
+    await player.setMovementPermission(true);
+    await player.setCameraPermission(true);
+    await player.runCommandAsync('fug @s remove test')
+    await player.runCommandAsync('fug @s remove fog')
+    await player.runCommandAsync('fog @s push dest:custom_fog test');
 });
