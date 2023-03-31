bfe9f15ae22473fd06f69aac0c3da3b4a9cdb1a6
Remember M9 <55829656+Myselfnt@users.noreply.github.com>
2023-03-30 21:38:47 -0700

    island spawn_point

--- packs/BP/scripts/config.js
+++ packs/BP/scripts/config.js
@@ -2,6 +2,6 @@ import { GameMode } from "@minecraft/server";
 
 export const config = {
     defualt_game_mode: GameMode.adventure,
-    default_spawn_point: { x: 88, y: 75, z: 88 }
+    default_spawn_point: { x: -729, y: 172, z: -827 }
 };
 export default config;
