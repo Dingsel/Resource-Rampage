acf31b4925ea3257ff5b26b54af216af3319f2cf
BigChungus21220 <109574168+BigChungus21220@users.noreply.github.com>
2023-04-01 00:27:03 -1000

    Update index.js

--- packs/BP/scripts/gameplay/runtime/index.js
+++ packs/BP/scripts/gameplay/runtime/index.js
@@ -3,5 +3,6 @@ export * from './player/import.js';
 export * from './towers/import.js';
 export * from './form/import.js';
 export * from './bossbar.js'
+export * from './player/gameStart.js'
 
 overworld.runCommandAsync("/function onStart").catch(errorHandle);
